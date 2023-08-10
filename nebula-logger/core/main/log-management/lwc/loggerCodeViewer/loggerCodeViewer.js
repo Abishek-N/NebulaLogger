34c25945fc6868a25fac9513eed77a935add7f58
Jonathan Gillespie <jonathan.c.gillespie@gmail.com>
2023-08-10 14:26:03 -0400

    WIP

--- nebula-logger/core/main/log-management/lwc/loggerCodeViewer/loggerCodeViewer.js
+++ nebula-logger/core/main/log-management/lwc/loggerCodeViewer/loggerCodeViewer.js
@@ -15,15 +15,72 @@ export default class LoggerCodeViewer extends LightningElement {
 
     isLoaded = false;
 
-    renderedCallback() {
+    _Prism;
+
+    async renderedCallback() {
         if (this.isLoaded) {
             return;
         }
 
-        Promise.all([loadScript(this, loggerStaticResources + '/prism.js'), loadStyle(this, loggerStaticResources + '/prism.css')]).then(() => {
+        // console.log('>>> aaaaaand setting innerHTML');
+        // await Promise.resolve(() => {
+        //     container.innerHTML =
+        //     // data-line and data-line-offset are effectively the same thing within Prism...
+        //     // but the core Prism code uses data-start for line numbers,
+        //     // and the line-highlight plugin uses data-line-offset for highlighting a line number
+        //     // (╯°□°）╯︵ ┻━┻
+        //     `<pre data-start="${this.startingLineNumber}" data-line="${this.targetLineNumber}" data-line-offset="${this.targetLineNumber}">` +
+        //     `<code class="language-${this.language}">${this.code}</code>` +
+        //     `</pre>`;
+        // });
+        // // await Promise.resolve();
+        // console.log('>>> aaaaaand set innerHTML', container.innerHTML);
+
+        // await new Promise(async (resolve, reject) => {
+        //     console.log('>>> setting innerHTML');
+        //     container.innerHTML =
+        //         // data-line and data-line-offset are effectively the same thing within Prism...
+        //         // but the core Prism code uses data-start for line numbers,
+        //         // and the line-highlight plugin uses data-line-offset for highlighting a line number
+        //         // (╯°□°）╯︵ ┻━┻
+        //         `<pre data-start="${this.startingLineNumber}" data-line="${this.targetLineNumber}" data-line-offset="${this.targetLineNumber}">` +
+        //         `<code class="language-${this.language}">${this.code}</code>` +
+        //         `</pre>`;
+        //     await Promise.resolve();
+        //     console.log('>>> set innerHTML', container.innerHTML);
+
+        //     return resolve(true);
+        // }).then(() => {
+        //     console.log('>>> starting to load script & style');
+        //     // return Promise.all([loadScript(this, loggerStaticResources + '/prism.js'), loadStyle(this, loggerStaticResources + '/prism.css')])
+        //     return Promise.all([loadScript(this, loggerStaticResources + '/prism.js'), loadStyle(this, loggerStaticResources + '/prism.css')]).then(() => {
+        //         console.log('>>> Prism', Prism);
+        //         Prism.highlightAll();
+        //         this.isLoaded = true;
+        //     })
+        // // }).then((args) => {
+        // //     Prism.highlightAll();
+        // //     this.isLoaded = true;
+        // })
+
+        // const container = this.template.querySelector('.prism-viewer');
+
+        //     // eslint-disable-next-line @lwc/lwc/no-inner-html
+        //     container.innerHTML =
+        //         // data-line and data-line-offset are effectively the same thing within Prism...
+        //         // but the core Prism code uses data-start for line numbers,
+        //         // and the line-highlight plugin uses data-line-offset for highlighting a line number
+        //         // (╯°□°）╯︵ ┻━┻
+        //         `<pre data-start="${this.startingLineNumber}" data-line="${this.targetLineNumber}" data-line-offset="${this.targetLineNumber}">` +
+        //         `<code class="language-${this.language}">${this.code}</code>` +
+        //         `</pre>`;
+
+        //         await Promise.resolve();
+
+        Promise.all([loadStyle(this, loggerStaticResources + '/prism.css'), loadScript(this, loggerStaticResources + '/prism.js')]).then(() => {
             const container = this.template.querySelector('.prism-viewer');
 
-            // eslint-disable-next-line @lwc/lwc/no-inner-html
+            // // eslint-disable-next-line @lwc/lwc/no-inner-html
             container.innerHTML =
                 // data-line and data-line-offset are effectively the same thing within Prism...
                 // but the core Prism code uses data-start for line numbers,
@@ -34,8 +91,15 @@ export default class LoggerCodeViewer extends LightningElement {
                 `</pre>`;
 
             // eslint-disable-next-line no-undef
+            this._Prism = Prism;
+            console.log('>>> this._Prism', this._Prism);
+            console.log('>>> this._Prism.highlightAll', this._Prism.highlightAll);
             Prism.highlightAll();
             this.isLoaded = true;
-        });
+            // setTimeout(() => this._Prism.highlightAll(), 0);
+        //     return Prism;
+        // }).then((Prism) => {
+        //     Prism.highlightAll();
+        })
     }
 }
